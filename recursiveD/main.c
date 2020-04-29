779f29a0343b2b7675eb32863f34b037cb4a5ea6
Andrew Cheng <ac1792@cd.cs.rutgers.edu>
2020-04-28 20:23:42 -0400

    Added code libraries: recursiveD and stringFunc

---
+++ recursiveD/main.c
@@ -0,0 +1,11 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<errno.h>
+#include"stringFunc.h"
+#include"recursiveD.h"
+
+int main(int argc, char** argv){
+    
+    recursivePrintDir("./");
+    return 0;
+}
---
+++ recursiveD/stringFunc.h
@@ -0,0 +1,9 @@
+#ifndef stringFunc
+#define stringFunc
+
+int compareString(char*, char*);
+char* prependToStr(char*, char*);
+char* appendToStr(char*, char*);
+char* mallocStr(int);
+
+#endif
