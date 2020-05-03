b94fb4f58591a0fe8a916f5396d96fa20b1507fb
John Yager <johnyager02@gmail.com>
2020-05-03 02:25:09 -0400

    added config

--- merged/client/clientSocket.c
+++ merged/client/clientSocket.c
@@ -3,7 +3,9 @@
 #include <stdlib.h> 
 #include <string.h> 
 #include <sys/socket.h> 
-int makeSocket();
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include <netdb.h> 
 #include <stdio.h> 
@@ -16,31 +18,36 @@ int makeSocket();
 #include "../manifestFunc.h"
 #include "../WTF.h"
 #define MAX 80 
-#define PORT 8080 
 #define SA struct sockaddr 
-void func(int sockfd) 
-{ 
-    char buff[MAX]; 
-    int n; 
-    for (;;) { 
-        bzero(buff, sizeof(buff)); 
-        printf("Enter the string : \n"); 
-        n = 0; 
-        while ((buff[n++] = getchar()) != '\n') 
-            ; 
-        write(sockfd, buff, sizeof(buff)); 
-        bzero(buff, sizeof(buff)); 
-        read(sockfd, buff, sizeof(buff)); 
-        printf("From Server : %s", buff); 
-        if ((strncmp(buff, "exit", 4)) == 0) { 
-            printf("Client Exit...\n"); 
-            break; 
-        } 
-    } 
-} 
+
   
-int main() 
-{ 
+int main(int argc, char** argv) { 
+    char* command = argv[1];
+
+    char IP[40];
+    int PORT;
+    if(compareString("configure", command) == 0){
+        if(argc != 4){
+            printf("Error: config command takes IP and port as additional arguments\n");
+            return 1;
+        }
+        int configFile = open("../.Config", O_WRONLY | O_CREAT, 00644);
+        char* configString = malloc(strlen(argv[2]) + 2);
+        strcpy(configString, argv[2]);
+        configString[strlen(argv[2])] = ' ';
+        configString[strlen(argv[2]) + 1] = '\0';
+        configString = appendToStr(configString, argv[3]);
+        write(configFile, configString, strlen(configString));
+        printf("Configuration file set up. This IP address and Port will be used for future commands.");
+        return 0;
+    }
+    else{
+        //attempt to open Config file
+        char* config = getFileContents("../.Config");
+
+        sscanf(config, "%s %d", &IP, &PORT);
+        printf("%s %d\n", IP, PORT);
+    }
     int connfd; 
     struct sockaddr_in servaddr, cli; 
   
@@ -50,13 +57,15 @@ int main()
         printf("socket creation failed...\n"); 
         exit(0); 
     } 
-    else
+    else{
         printf("Socket successfully created..\n"); 
+    }
+
     bzero(&servaddr, sizeof(servaddr)); 
   
     // assign IP, PORT 
     servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1"); 
+    servaddr.sin_addr.s_addr = inet_addr(IP); 
     servaddr.sin_port = htons(PORT); 
   
     // connect the client socket to server socket 
@@ -68,8 +77,6 @@ int main()
         printf("connected to the server..\n"); 
   
     // function for chat 
-    func(sockfd); 
-  
     // close the socket 
     close(sockfd); 
 } 
